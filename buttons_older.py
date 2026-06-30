5e8cc264377ee02c345ef12d57c5d3e07a995003
DigTheDoug <digthedoug@hotmail.com>
2026-06-29 21:50:35 -0400

    buttons.py update for newer os's

--- buttons_older.py
+++ buttons_older.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-# This version of the buttons.py script is for bookworm (os 12)
-# and later which use the raspi-gpio command to control the GPIO pins 
-# instead of using the RPi.GPIO library.
+# For older versions of the OS (11 and earlier) which use the RPi.GPIO 
+# library to control the GPIO pins instead of using the raspi-gpio command.
 
 import logging
 import RPi.GPIO as GPIO
@@ -14,7 +13,7 @@ def turnOnScreen():
     # Enable audio
     os.system("raspi-gpio set 19 op a5")
     # Turn on screen backlight
-    os.system("raspi-gpio set 18 op dl")
+    GPIO.output(18, GPIO.HIGH)
 
 
 def turnOffScreen():
@@ -22,7 +21,7 @@ def turnOffScreen():
     # Mute audio
     os.system("raspi-gpio set 19 ip")
     # Turn off screen backlight
-    os.system("raspi-gpio set 18 op dh")
+    GPIO.output(18, GPIO.LOW)
 
 
 def main():
@@ -32,15 +31,17 @@ def main():
     GPIO.setmode(GPIO.BCM)
     # Set pin 26 as input to monitor button press
     GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    # Set pin 18 as output to control screen backlight
+    GPIO.setup(18, GPIO.OUT)
 
-    turnOnScreen()
-    screen_on = True
+    turnOffScreen()
+    screen_on = False
 
     while True:
         # If you are having and issue with the button doing the opposite of what you want
         # IE Turns on when it should be off, change this line to:
-        # input = GPIO.input(26)
-        input = not GPIO.input(26)
+        # input = not GPIO.input(26)
+        input = GPIO.input(26)
         if input != screen_on:
             screen_on = input
             if screen_on:
